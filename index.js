5834ac7d91042c78e7b492215aee66fb65eb8ff7
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-03 05:53:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -84,6 +84,156 @@ function deepExtracover(run){
     
   }
 
+/* squreLeg */
+
+  function squreLeg(run){
+  
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f1");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "squreLeg-ball 4s";
+  }
+  else{
+    ball.style.animation = "squreLeg-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "squreLeg-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+  }
+
+
+
+/*  point */
+
+  function point(run){
+  
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f9");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "point-ball 4s";
+  }
+  else{
+    ball.style.animation = "point-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "point-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+  }
+
+/*  midOff */
+
+  function midOff(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f3");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "midOff-ball 4s";
+  }
+  else{
+    ball.style.animation = "midOff-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "midOff-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+
+/*  midOn */
+
+  function midOn(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f4");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "midOn-ball 4s";
+  }
+  else{
+    ball.style.animation = "midOn-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "midOn-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+
+
+/* fineLeg */
+
+  function fineLeg(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f5");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "fineLeg-ball 4s";
+  }
+  else{
+    ball.style.animation = "fineLeg-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "fineLeg-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
+
+/* gully */
+
+  function gully(run){
+   
+  const ball = document.getElementById("ball");
+  const filder = document.getElementById("f6");
+  
+  if(run < 4 || !run){
+    ball.style.animation = "gully-ball 4s";
+  }
+  else{
+    ball.style.animation = "gully-ball-boundary 4s";
+  }
+  
+  filder.style.animation = "gully-filder 5s";
+  
+  setTimeout(function() {
+    ball.style.animation = "";
+  }, 4000);
+  setTimeout(function() {
+    filder.style.animation = "";
+  }, 5000);
+
+  }
 
 
 
